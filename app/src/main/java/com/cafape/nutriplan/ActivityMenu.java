6f7ec29981b72dc750aee4df06362d7c04907dba
GEOLAB IAMB <geolab.iamb@gmail.com>
2021-01-01 20:30:49 +0100

    - Draft graphics db import/export

--- app/src/main/java/com/cafape/nutriplan/ActivityMenu.java
+++ app/src/main/java/com/cafape/nutriplan/ActivityMenu.java
@@ -3,9 +3,12 @@ package com.cafape.nutriplan;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,6 +18,7 @@ public class ActivityMenu extends AppCompatActivity
     private ConstraintLayout activitymanu_constraintLayout_card_calc;
     private ConstraintLayout activitymanu_constraintLayout_card_patient;
     private ConstraintLayout activitymanu_constraintLayout_card_appointment;
+    private ConstraintLayout activitymanu_constraintLayout_card_importexport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +34,7 @@ public class ActivityMenu extends AppCompatActivity
         activitymanu_constraintLayout_card_calc = findViewById(R.id.activitymanu_constraintLayout_card_calc);
         activitymanu_constraintLayout_card_patient = findViewById(R.id.activitymanu_constraintLayout_card_patient);
         activitymanu_constraintLayout_card_appointment = findViewById(R.id.activitymanu_constraintLayout_card_appointment);
+        activitymanu_constraintLayout_card_importexport = findViewById(R.id.activitymanu_constraintLayout_card_importexport);
     }
 
     public void setListeners() {
@@ -57,6 +62,17 @@ public class ActivityMenu extends AppCompatActivity
             public void onClick(View view) {
                 Intent intent_goToActivity = new Intent(context, ActivityAppointments.class);
                 startActivity(intent_goToActivity);
+            }
+        });
+        activitymanu_constraintLayout_card_importexport.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alert_importexport = new AlertDialog.Builder(ActivityMenu.this);
+                LayoutInflater factory = LayoutInflater.from(ActivityMenu.this);
+                final View view_importexport = factory.inflate(R.layout.dialog_importexport, null);
+                alert_importexport.setView(view_importexport);
+                alert_importexport.show();
             }
         });
     }
